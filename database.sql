f93b47aca4105958090f673f92d222313f24df4b
Hunter Rancourt <skwid138@gmail.com>
2017-09-18 09:10:57 -0500

    updated SQL

--- database.sql
+++ database.sql
@@ -4,9 +4,9 @@
 CREATE TABLE tasks (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(30),
-	description VARCHAR(200),
+	description VARCHAR(200), -- could also use TEXT
 	status BOOLEAN DEFAULT false,
 	due VARCHAR(10),
-	created DATE DEFAULT CURRENT_DATE,
-	completed DATE
+	created DATE DEFAULT CURRENT_DATE, -- this may not work, I changed it using the gui
+	completed DATE -- this may not work, I changed it using the gui
 );
